c54ea2c8a83f1013b46ffdcbf260e1dc632fb290
stiven-hidri <hidristiven@gmail.com>
2025-01-04 17:33:45 +0100

    clean repo

--- figimodi/labs/lab01/src/ex3.py
+++
@@ -1,8 +0,0 @@
-#Nome, Cognome, Citta, Data
-
-class Person:
-    def __init__(self, name, surname, birthplace, birthdate):
-        self.name = name
-        self.surname = surname
-        self.birthplace = birthplace
-        self.birthdate = birthdate
--- figimodi/project/README.md
+++
@@ -1,3 +0,0 @@
-## Link of the latex project
-
-[Latex](https://www.overleaf.com/project/64624f9c0cda3bcaa5865e16)
